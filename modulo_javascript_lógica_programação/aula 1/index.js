f09779c85ecb158868c1e7f03aca45d746152acc
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2023-11-03 21:42:17 -0300

    update Extrutura para modulos

---
+++ modulo_javascript_lógica_programação/aula 1/index.js
@@ -0,0 +1,13 @@
+/*operadores de comparação
+* > maior que
+* >= maior ou igual a
+* < menor que 
+* <= menor ou igual a
+* == igualdade(valor)
+* === igualdade estrita(valor e tipo) 
+* != diferente(valor)
+* !== diferente estrito(valor e tipo) 
+sempre vao retornar boolean
+*/
+const compare= (10 >= 5 )
+console.log(compare)
