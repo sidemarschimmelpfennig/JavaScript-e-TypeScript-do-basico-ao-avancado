27dfa069a41075c8adbf92ab6de1cf455a5d99da
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2023-12-01 22:35:06 -0300

    aula#1

---
+++ modulo 8 - Javascript Tooling e ES6 Modules/aula 1 - babel e can in use/index.js
@@ -0,0 +1,16 @@
+let arr = {
+    vale : "",
+    teste : '',
+    newtest: "",
+    component: ""
+}
+
+const newArr = []
+
+for(value in arr){
+    newArr.push(value)
+}
+for (let index = 0; index < newArr.length; index++) {
+    console.log(index)
+    
+}
