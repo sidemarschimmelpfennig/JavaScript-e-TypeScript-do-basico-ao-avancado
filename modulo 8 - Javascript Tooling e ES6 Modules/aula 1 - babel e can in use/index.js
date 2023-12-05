26f4a9a4f295802ba002e065c275a91aa998a484
Sidemar Schimmelpfennig Junior <90000000+sidemarschimmelpfennig@users.noreply.github.com>
2023-12-05 05:59:21 -0300

    aula#1 e aula#2

--- modulo 8 - Javascript Tooling e ES6 Modules/aula 1 - babel e can in use/index.js
+++ modulo 8 - Javascript Tooling e ES6 Modules/aula 1 - babel e can in use/index.js
@@ -1,16 +1,17 @@
-let arr = {
-    vale : "",
-    teste : '',
-    newtest: "",
-    component: ""
+const nome = 'junior'
+const obj = {...nome}
+const newObj = {...obj}
+const newwObj = {...newObj}
+
+console.log(newObj)
+console.log(obj)
+console.log(newwObj)
+
+class Pessoa {
+    constructor(nome, sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
 }
 
-const newArr = []
-
-for(value in arr){
-    newArr.push(value)
-}
-for (let index = 0; index < newArr.length; index++) {
-    console.log(index)
-    
-}
+//npx babel (arquivo origem) -o (arquivo destino) --presets=@babel/env
