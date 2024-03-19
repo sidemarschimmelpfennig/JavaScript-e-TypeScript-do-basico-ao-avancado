d745be8c6300d5bfe528bf8911dba30ec5fd1d40
sidemar <sidemarschimmelpfennig@gmail.com>
2024-03-19 16:58:52 -0300

    header create from application

--- js/react/react_base/src/App.jsx
+++ js/react/react_base/src/App.jsx
@@ -1,10 +1,12 @@
 import GlobalStyle from './styles/GlobalStyle';
 
 import Login from './components/Login';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
     </>
